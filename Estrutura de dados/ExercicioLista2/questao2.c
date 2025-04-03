4709e2945327d58d71c56afa9d3adb26aba3b977
Antônio Leite <128553621+xOmilk@users.noreply.github.com>
2025-04-03 16:35:05 -0300

    Create questao2.c

---
+++ Estrutura de dados/ExercicioLista2/questao2.c
@@ -0,0 +1,9 @@
+/*
+Faça um algoritmo que implemente uma função que leia uma sequência
+
+de NOMES informados pelo usuário, salve-os numa lista simples-
+mente encadeada circular, ordene-os usando um dos algoritmos de
+
+ordenação visto em sala e mostre-os em ordem decrescente.
+*/
+
